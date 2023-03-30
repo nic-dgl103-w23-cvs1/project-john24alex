8145ccd7f09ee1d3cac67919ca59d0a669be186a
John <john24alex@yahoo.com>
2023-03-29 19:47:48 -0700

    JS ajustment
    JS ajustment

--- Js/main.js
+++ Js/main.js
@@ -4,12 +4,22 @@ $(document).ready(function(){
 
   $("#btnMain").click(function(){
     $(".menuBar").toggleClass("activeMenu");
-   
+
+
   });
 
 
+//Animations Hero Banner
 
+//init state
+
+
+$(".bg_hero").animate({'right': '64%'}, 600);
+
+
+$(".hero div ").animate({'opacity': '1'}, 400);
    
+
 
 
 
